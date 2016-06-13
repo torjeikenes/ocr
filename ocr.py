1ec720fa9353b550cd6b4e82cca4a289e3bac84f
torjeikenes <torje.n.eikenes@gmail.com>
2016-06-13 22:55:05 +0200

    Can detect phone number

--- ocr.py
+++ ocr.py
@@ -6,4 +6,28 @@ script, filename = argv
 text = pytesseract.image_to_string(Image.open(filename))
 
 lines = text.split('\n')
+phone = 0
+def isNumber(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def isPhone(n):
+    num = ""
+    for i in n:
+        if((i != "+") or (i != " ")):
+            num+=i
+    print num
+    return num
+
+for line in lines:
+    num = line.replace("+", "")
+    num2 = num.replace(" ", "")
+    if(isNumber(isPhone(num2))):
+        phone = line
+
 print lines
+print phone
